bb23592e5ac76b8a350f742e99ff76e3f2b0b98c
niceman1234man <yihunietarekegn18@gmail.com>
2025-01-31 10:03:01 -0800

    note card

--- frontend/src/Components/NoteCard.jsx
+++ frontend/src/Components/NoteCard.jsx
@@ -1,79 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TiPinOutline } from "react-icons/ti";
 import { MdModeEditOutline } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 
 
-function NoteCard() {
+function NoteCard({
+   title,
+   date,
+   content,
+   tags,
+   isPinned,
+   onEdit,
+   onDelete,
+   onPinNote,
+   
+}) {
+
+   
   return (
-    <div className='container mx-auto mt-8'>
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-     <div className='border shadow-md p-4'>
-        <h3 className='text-lg font-semibold '>Title of Note</h3>
-        <div className='py-2 flex justify-between'>
-        <p className='text-xs '>{new Date().toLocaleString()} </p>
-        <TiPinOutline size={20}/>
-        </div>
-        <p>You may be wondering, "What is a remote manager?" They are any senior employee responsible for directing a team who work entirely or partly from a different physical location. </p>
-        <div className='py-2 flex justify-between'>
-        <p >tages# tag2# </p>
-        <p className='flex'><MdModeEditOutline className='mr-2'/> <MdDelete/></p>
-        </div>
-     </div>
 
      <div className='border shadow-md p-4'>
-        <h3 className='text-lg font-semibold '>Title of Note</h3>
+        <h3 className='text-lg font-semibold '>{title}</h3>
         <div className='py-2 flex justify-between'>
-        <p className='text-xs '>{new Date().toLocaleString()} </p>
-        <TiPinOutline size={20}/>
+        <p className='text-xs '>{date} </p>
+        <TiPinOutline size={20} className={`cursor-pointer ${isPinned? 'text-blue-400':''}`}/>
         </div>
-        <p>You may be wondering, "What is a remote manager?" They are any senior employee responsible for directing a team who work entirely or partly from a different physical location. </p>
+        <p>{content} </p>
         <div className='py-2 flex justify-between'>
-        <p >tages# tag2# </p>
-        <p className='flex'><MdModeEditOutline className='mr-2'/> <MdDelete/></p>
-        </div>
-     </div>
-
-     <div className='border shadow-md p-4'>
-        <h3 className='text-lg font-semibold '>Title of Note</h3>
-        <div className='py-2 flex justify-between'>
-        <p className='text-xs '>{new Date().toLocaleString()} </p>
-        <TiPinOutline size={20}/>
-        </div>
-        <p>You may be wondering, "What is a remote manager?" They are any senior employee responsible for directing a team who work entirely or partly from a different physical location. </p>
-        <div className='py-2 flex justify-between'>
-        <p >tages# tag2# </p>
-        <p className='flex'><MdModeEditOutline className='mr-2'/> <MdDelete/></p>
+        <p >{tags}</p>
+        <p className='flex'><MdModeEditOutline className='mr-2 cursor-pointer' onClick={onEdit}/> <MdDelete onClick={onDelete}  className='hover:text-red-600 cursor-pointer'/></p>
         </div>
      </div>
-
-     <div className='border shadow-md p-4'>
-        <h3 className='text-lg font-semibold '>Title of Note</h3>
-        <div className='py-2 flex justify-between'>
-        <p className='text-xs '>{new Date().toLocaleString()} </p>
-        <TiPinOutline size={20}/>
-        </div>
-        <p>You may be wondering, "What is a remote manager?" They are any senior employee responsible for directing a team who work entirely or partly from a different physical location. </p>
-        <div className='py-2 flex justify-between'>
-        <p >tages# tag2# </p>
-        <p className='flex'><MdModeEditOutline className='mr-2'/> <MdDelete/></p>
-        </div>
-     </div>
-
-     <div className='border shadow-md p-4'>
-        <h3 className='text-lg font-semibold '>Title of Note</h3>
-        <div className='py-2 flex justify-between'>
-        <p className='text-xs '>{new Date().toLocaleString()} </p>
-        <TiPinOutline size={20}/>
-        </div>
-        <p>You may be wondering, "What is a remote manager?" They are any senior employee responsible for directing a team who work entirely or partly from a different physical location. </p>
-        <div className='py-2 flex justify-between'>
-        <p >tages# tag2# </p>
-        <p className='flex'><MdModeEditOutline className='mr-2'/> <MdDelete/></p>
-        </div>
-     </div>
-    </div>
-    </div>
+    
   )
 }
 
