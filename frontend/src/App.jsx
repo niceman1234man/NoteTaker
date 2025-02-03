30808774741e9288e3a1b72eec65323a9d6169d1
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-03 11:11:16 -0800

    packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import Navnar from './Components/Navnar'
 import { Routes,Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import AddNote from './Components/AddNote'
+
 
 function App() {
   return (
     <>
-    <Navnar/>
+  
     <Routes>
       <Route path='/' element={<Dashboard/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/add' element={<AddNote/>}/>
     </Routes>
     </>
   )
