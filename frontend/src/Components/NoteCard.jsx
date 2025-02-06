941d930552dd3c0571cde294c25b158965cba94c
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-06 11:16:45 -0800

    note card

--- frontend/src/Components/NoteCard.jsx
+++ frontend/src/Components/NoteCard.jsx
@@ -16,18 +16,25 @@ function NoteCard({
    
 }) {
 
-   
+   const formatDate = (dateString) => {
+      const options = { year: 'numeric', month: 'short', day: '2-digit' };
+      const date = new Date(dateString);
+      return date.toLocaleDateString('en-US', options).replace(',', '');
+  };
+  
+  const formattedDate = formatDate(date);
+ 
   return (
 
      <div className='border shadow-md p-4'>
         <h3 className='text-lg font-semibold '>{title}</h3>
         <div className='py-2 flex justify-between'>
-        <p className='text-xs '>{date} </p>
+        <p className='text-xs '>{formattedDate} </p>
         <TiPinOutline size={20} className={`cursor-pointer ${isPinned? 'text-blue-400':''}`}/>
         </div>
         <p>{content} </p>
         <div className='py-2 flex justify-between'>
-        <p >{tags}</p>
+        <p >{tags.map(item =>`#${item} `)}</p>
         <p className='flex'><MdModeEditOutline className='mr-2 cursor-pointer' onClick={onEdit}/> <MdDelete onClick={onDelete}  className='hover:text-red-600 cursor-pointer'/></p>
         </div>
      </div>
