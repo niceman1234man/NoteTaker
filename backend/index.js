22f521e3bef22f89f029b95c9e858233173403f7
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-06 11:26:41 -0800

    index

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDb } from './config/db.js';
+import { router } from './router/note.router.js';
+import { userRouter } from './router/user.router.js';
 dotenv.config();
 const app =express();
+app.use(express.json());
+app.use(cors({
+    origin:"*"
+}));
+app.use('/user/',userRouter);
+app.use('/note/',router);
 connectDb();
+
 app.listen(5000,()=>{
     console.log("server running")
 });
