a660264084e99a00e825085bd587ddf3fd96764b
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-06 09:41:05 -0800

    user router

--- backend/router/user.router.js
+++ backend/router/user.router.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { createAccount,getUser,login } from '../controller/user.controller.js';
-
+import { authenticateToken } from '../utilities.js';
 export const userRouter=express.Router();
 userRouter.post('/create-account',createAccount);
 userRouter.post('/login',login);
-userRouter.get('/get-user',getUser);
+userRouter.get('/get-user',authenticateToken,getUser);
