20a52b54719f13dc0335f1945d6645f3b168ecf2
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-06 09:39:20 -0800

    utilities

--- backend/utilities.js
+++ backend/utilities.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 export const authenticateToken=(req,res,next)=>{
     const authHeader=req.headers['authorization'];
-    const token=authHeader && authHeader.splite(" ")[1];
+    const token=authHeader && authHeader.split(" ")[1];
     if(!token) return res.sendStatus(401);
     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
         if(err) return res.sendStatus(401);
