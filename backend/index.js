91e17a9eaafb1ccb914d07a20144407b2521c862
Yihunie Tarekegn <136583776+niceman1234man@users.noreply.github.com>
2025-02-06 22:53:14 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app =express();
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://note-taker-lemon.vercel.app",
     credentials: true,
     
 }));
