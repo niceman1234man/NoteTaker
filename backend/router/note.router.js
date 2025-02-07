6748aacdcea47186d493a7baed69a4110db03f37
niceman1234man <yihunietarekegn18@gmail.com>
2025-02-06 17:03:31 -0800

    note router

--- backend/router/note.router.js
+++ backend/router/note.router.js
@@ -2,9 +2,9 @@ import express from 'express'
 import { authenticateToken } from '../utilities.js';
 import { addNote,editNote,deleteNote,allNotes,editPin, searchNote} from '../controller/note.controller.js';
 export const router=express.Router();
-router.post('/add-note',authenticateToken,addNote)
-router.put('/edit-note/:id',authenticateToken,editNote)
-router.put('/pin-note/:id',authenticateToken,editPin)
-router.get('/get-notes',authenticateToken,allNotes)
-router.get('/search-notes',authenticateToken,searchNote)
-router.delete('/delete-note/:id',authenticateToken,deleteNote)
+router.post('/add-note',authenticateToken,addNote);
+router.put('/edit-note/:id',authenticateToken,editNote);
+router.put('/pin-note/:id',authenticateToken,editPin);
+router.get('/get-notes',authenticateToken,allNotes);
+router.get('/search-note',authenticateToken,searchNote);
+router.delete('/delete-note/:id',authenticateToken,deleteNote);
